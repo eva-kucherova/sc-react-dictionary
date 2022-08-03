7f8274c19c3ace9283d91d834f5ad281a9115bc9
Zoia-K <evahekate@yahoo.com>
2022-08-03 14:58:37 +0300

    Created react App & added Bootstrap

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='container'>
+        <header className='App-header'>
+          <h1> Dictionary App </h1>
+          <button className='btn btn-primary'> Search </button>
+        </header>
+      </div>
+    </div>
+  );
+}
+
+export default App;

d63f240104eb09eb24c9c2e2684eb468a385fe94
Zoia-K <evahekate@yahoo.com>
2022-08-03 16:31:05 +0300

    Alegreya font installed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import './App.css';
+import Zoia_K from './Zoia_K.png';
 
-function App() {
+export default function App() {
   return (
     <div className='App'>
       <div className='container'>
         <header className='App-header'>
+          <img src={Zoia_K} alt='Zoia_K' className='img-fluid'></img>
+
           <h1> Dictionary App </h1>
-          <p>What is real meaning of</p>
-          <button className='btn btn-primary'> Search </button>
         </header>
+        <p>What is real meaning of</p>
+        <button className='btn btn-primary'> Search </button>
       </div>
     </div>
   );
 }
-
-export default App;

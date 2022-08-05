c5b2b3660b7b06c95ef7e75102e65de435593b18
Zoia-K <evahekate@yahoo.com>
2022-08-05 15:50:39 +0300

    Phonetics block added

--- src/Results.js
+++ src/Results.js
@@ -2,10 +2,20 @@ import React from 'react';
 import Meanings from './Meanings';
 
 export default function Results(props) {
+  console.log(props.results);
   if (props.results) {
     return (
       <div className='Results'>
         <h2>{props.results.word}</h2>
+        <a
+          href={props.results.phonetics[0].audio}
+          target='_blank'
+          rel='noreferrer'
+        >
+          Listen
+        </a>
+        <span>{props.results.phonetic}</span>
+
         {props.results.meanings.map(function (meanings, index) {
           return (
             <div key={index}>
